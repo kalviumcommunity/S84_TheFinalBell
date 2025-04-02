ba5cafd6003251b292ea46d14b7a74a3993e45fa
ANU-2524 <anusoni25.2006@gmail.com>
2025-04-02 19:01:36 +0530

    Added Update and Delete Option on EntityForm

--- Backend/route/entityRoutes.js
+++ Backend/route/entityRoutes.js
@@ -29,4 +29,38 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const entity = await Entity.findById(id);
+
+  if (!entity) {
+      return res.status(404).json({ message: "Entity not found" });
+  }
+
+  entity.title = req.body.title;
+  entity.description = req.body.description;
+
+  await entity.save();
+  res.json(entity);
+});
+
+
+// DELETE entity by ID
+router.delete("/:id", async (req, res) => {
+  try {
+
+    const {id} = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "ID is required" });
+    }
+    const entity = await Entity.findByIdAndDelete(id);
+    if (!entity) {
+      return res.status(404).json({ message: "Entity not found" });
+    }
+    res.status(200).json({ message: "Entity deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting entity" });
+  }
+});
+
 module.exports = router;
